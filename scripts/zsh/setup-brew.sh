1c11973d1e4bd377fa9760804b284ee73221d26f
jteng2127 <39561523+jteng2127@users.noreply.github.com>
2024-06-25 18:03:22 +0800

    Update setup-brew.sh

--- scripts/zsh/setup-brew.sh
+++ scripts/zsh/setup-brew.sh
@@ -5,6 +5,7 @@ if command -v brew > /dev/null 2>&1; then
   echo "brew is already installed"
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 fi
 
 # zsh is already installed on macOS
@@ -12,11 +13,9 @@ if command -v zsh > /dev/null 2>&1; then
   echo "zsh is already installed"
 else
   brew install zsh
-  chsh -s $(which zsh)
+  command -v zsh >> /etc/shells
+  sudo chsh -s $(command -v zsh) "${USER}"
 fi
 
 # for `zsh` module
 brew install stow fzf zoxide
-
-# optional
-brew install 
